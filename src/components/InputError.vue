ef07f57e1e19e31380ee38328bd343ee0aca20ba
Patryk <patrykowo@gmail.com>
2022-10-20 23:42:08 +0200

    Add inputs' validation. Add separate component to handle display errors.

---
+++ src/components/InputError.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+export default {
+  name: 'InputError',
+  props: {
+    text: String,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  color: red;
+  text-align: center;
+}
+</style>

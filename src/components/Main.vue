27905a1f34147774f464f1817fce5ba2067a7754
Patryk <patrykowo@gmail.com>
2022-10-29 23:19:47 +0200

    Add carousel with some styles and js.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,66 +1,82 @@
 <template>
   <main>
-    <div class="scene__wrap fade-in">
-      <div class="scene">
-        <div class="cube">
-          <div class="cube__face cube__face--front">
-            <div class="text-bg-cover">
-              <h4>Norella Beauty Studio</h4>
+    <div class="wrapper">
+      <div class="scene__wrap fade-in">
+        <div class="scene">
+          <div class="cube">
+            <div class="cube__face cube__face--front">
+              <div class="text-bg-cover">
+                <h4>Norella Beauty Studio</h4>
+              </div>
             </div>
+            <div class="cube__face cube__face--back">
+              <p>Mobile-first, fully responsive CMS/CRUD web application. Build with Laravel and Bootstrap. <span>Slow loading might be caused due to shared hosting server.</span></p>
+            </div>
+            <div class="cube__face cube__face--right">
+              <h4>Technologies</h4>
+              <ul>
+                <li>PHP</li>
+                <li>MySQL</li>
+                <li>CSS</li>
+                <li>Laravel</li>
+                <li>Bootstrap</li>
+              </ul>
+            </div>
+            <div class="cube__face cube__face--left">
+              <a href="https://devpat.online/projects/norella/public/index.php" target="_blank">
+                <font-awesome-icon :icon="['fa-solid', 'fa-globe']"></font-awesome-icon>
+              </a>
+              <a href="https://github.com/Piecuch-Patryk/norella" target="_blank">
+                <font-awesome-icon :icon="['fa-brands', 'fa-square-github']" />
+              </a>
+            </div>
+            <div class="cube__face cube__face--top"></div>
+            <div class="cube__face cube__face--bottom"></div>
           </div>
-          <div class="cube__face cube__face--back">
-            <p>Mobile-first, fully responsive CMS/CRUD web application. Build with Laravel and Bootstrap. <span>Slow loading might be caused due to shared hosting server.</span></p>
-          </div>
-          <div class="cube__face cube__face--right">
-            <h4>Technologies</h4>
-            <ul>
-              <li>PHP</li>
-              <li>MySQL</li>
-              <li>CSS</li>
-              <li>Laravel</li>
-              <li>Bootstrap</li>
-            </ul>
-          </div>
-          <div class="cube__face cube__face--left">
-            <a href="https://devpat.online/projects/norella/public/index.php" target="_blank">
-              <font-awesome-icon :icon="['fa-solid', 'fa-globe']"></font-awesome-icon>
-            </a>
-            <a href="https://github.com/Piecuch-Patryk/norella" target="_blank">
-              <font-awesome-icon :icon="['fa-brands', 'fa-square-github']" />
-            </a>
-          </div>
-          <div class="cube__face cube__face--top"></div>
-          <div class="cube__face cube__face--bottom"></div>
         </div>
       </div>
+      <div class="slide-in">
+        <ul class="cube__nav">
+          <li>
+            <input id="front" type="radio" name="rotate-cube-side" value="front" checked />
+            <label for="front">Norella</label>
+          </li>
+          <li>
+            <input id="right" type="radio" name="rotate-cube-side" value="right" />
+            <label for="right">Technologies</label>
+          </li>
+          <li>
+            <input id="back" type="radio" name="rotate-cube-side" value="back" />
+            <label for="back">Description</label>
+          </li>
+          <li>
+            <input id="left" type="radio" name="rotate-cube-side" value="left" />
+            <label for="left">Links</label>
+          </li>
+          <li>
+            <input id="top" type="radio" name="rotate-cube-side" value="top" />
+            <label for="top">Mobile First</label>
+          </li>
+          <li>
+            <input id="bottom" type="radio" name="rotate-cube-side" value="bottom" />
+            <label for="bottom">GIF</label>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="slide-in">
-      <ul class="cube__nav">
-        <li>
-          <input id="front" type="radio" name="rotate-cube-side" value="front" checked />
-          <label for="front">Norella</label>
-        </li>
-        <li>
-          <input id="right" type="radio" name="rotate-cube-side" value="right" />
-          <label for="right">Technologies</label>
-        </li>
-        <li>
-          <input id="back" type="radio" name="rotate-cube-side" value="back" />
-          <label for="back">Description</label>
-        </li>
-        <li>
-          <input id="left" type="radio" name="rotate-cube-side" value="left" />
-          <label for="left">Links</label>
-        </li>
-        <li>
-          <input id="top" type="radio" name="rotate-cube-side" value="top" />
-          <label for="top">Mobile First</label>
-        </li>
-        <li>
-          <input id="bottom" type="radio" name="rotate-cube-side" value="bottom" />
-          <label for="bottom">GIF</label>
-        </li>
-      </ul>
+    <div class="wrapper">
+      <div class="container fade-in">
+        <div class="carousel" :style="rotateY">
+          <div class="item a">A</div>
+          <div class="item b">B</div>
+          <div class="item c">C</div>
+          <div class="item d">D</div>
+          <div class="item e">E</div>
+          <div class="item f">F</div>
+        </div>
+      </div>
+      <button @click="rotate('next')" class="next">Next</button>
+      <button @click="rotate('prev')" class="prev">Prev</button>
     </div>
   </main>
 </template>
@@ -68,7 +84,21 @@
 <script>
 export default {
   name: 'Main',
+  data() {
+    return {
+      rotateVal: 0,
+    };
+  },
+  computed: {
+    rotateY () {
+      return { transform: `rotateY(${ this.rotateVal }deg)` };
+    }
+  },
   methods: {
+    rotate(direction) {
+      if(direction === 'next') this.rotateVal -= 60;
+      if(direction === 'prev') this.rotateVal += 60;
+    },
     cubeInit() {
       const cube = document.querySelector('.cube');
       const radioGroup = document.querySelector('.cube__nav');
@@ -97,9 +127,14 @@ export default {
 main {
   position: relative;
   z-index: 3;
-  display: flex;
-  flex-flow: column-reverse;
   padding: 2rem 2rem 10rem;
+  
+  .wrapper {
+    display: flex;
+    flex-flow: column-reverse;
+    position: relative;
+    margin-bottom: 10rem;
+  }
 }
 .scene__wrap {
   margin-top: 8rem;
@@ -342,9 +377,12 @@ input {
 
 @media (min-width: 768px) {
   main {
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
+
+    .wrapper {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+    }
   }
   .cube__nav {
     position: relative;
@@ -427,4 +465,77 @@ input {
     font-size: 2.5rem;
   }
 }
+
+.container {
+  margin: 0 auto;
+  width: 200px;
+  height: 200px;
+  position: relative;
+  perspective: 600px;
+}
+
+.carousel {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+  transition: transform 1s;
+}
+
+.item {
+  display: block;
+  position: absolute;
+  background: #000;
+  width: 200px;
+  height: 200px;
+  line-height: 200px;
+  font-size: 5em;
+  text-align: center;
+  color: #FFF;
+  opacity: 0.95;
+  border-radius: 10px;
+}
+
+.a {
+  transform: rotateY(0deg) translateZ(200px);
+  background: #ed1c24;
+}
+.b {
+  transform: rotateY(60deg) translateZ(200px);
+  background: #0072bc;
+}
+.c {
+  transform: rotateY(120deg) translateZ(200px);
+  background: #39b54a;
+}
+.d {
+  transform: rotateY(180deg) translateZ(200px);
+  background: #f26522;
+}
+.e {
+  transform: rotateY(240deg) translateZ(200px);
+  background: #630460;
+} 
+.f {
+  transform: rotateY(300deg) translateZ(200px);
+  background: #8c6239;
+}
+
+.next, .prev {
+  color: #444;
+  position: absolute;
+  bottom: -5rem;
+  padding: 1em 2em;
+  cursor: pointer;
+  border-radius: 5px;
+  border-top: 1px solid #FFF;
+  box-shadow: 0 5px 0 #999;
+  transition: box-shadow 0.1s, top 0.1s;
+}
+.next:hover, .prev:hover { color: #000; }
+.next:active, .prev:active {
+  box-shadow: 0 1px 0 #999;
+}
+.next { right: 5em; }
+.prev { left: 5em; }
 </style>

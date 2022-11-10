8d5931ba5a54189b8702e3b8e3fea74ad95f378f
Patryk <patrykowo@gmail.com>
2022-11-10 10:41:11 +0100

    Add some more responsive styles for tablet and desktop.

--- src/components/Hangman.vue
+++ src/components/Hangman.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="hangman">
-    <div>
+    <div class="text-wrap">
       <h3>Hangman Game</h3>
       <p>Single page, fully responsive JavaScript game build with Vue 3. Choose a category and difficulty level and let the game begin.</p>
       <p>Take a chance and don't hang yourself.</p>
-      <div class="link-wrap">
-        <a href="http://devpat.online/projects/hangman" target="_blank">Let's PLAY</a>
-      </div>
+      <a href="http://devpat.online/projects/hangman" target="_blank">Let's PLAY</a>
     </div>
     <div>
       <img src="@/assets/hangman/responsive.png" alt="Responsive game design showed on different devices.">
@@ -25,23 +23,61 @@ export default {
   color: #ddd;
   text-align: center;
 
-  h3 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-  }
-  p {
-    margin-bottom: 1rem;
-  }
+  .text-wrap {
+    padding-bottom: 2rem;
+    h3 {
+      font-size: 2rem;
+      margin-bottom: 1rem;
+    }
+    p {
+      margin-bottom: 1rem;
+    }
 
-  .link-wrap {
-    padding: 1rem;
+    .link-wrap {
+      padding: 1rem;
 
-    a {
-      color: #662D91;
+      a {
+        color: #662D91;
+      }
     }
   }
   img {
     width: 100%;
+  }
+}
+
+@media (min-width: 768px) {
+  .hangman {
+    max-width: 70%;
+  }
+}
+
+@media (min-width: 1000px) {
+  .hangman {
+    max-width: initial;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 8rem auto;
+
+    .text-wrap {
+      p {
+        width: 50%;
+        margin: 1rem auto 0 auto;
+
+        &:last-of-type {
+          margin-bottom: 2rem;
+        }
+      }
+    }
+    div {
+      width: 50%;
+      text-align: center;
+
+      img {
+        width: 80%;
+      }
+    }
   }
 }
 </style>

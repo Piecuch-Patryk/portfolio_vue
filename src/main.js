7851e9cd68b818245e607a2286b419331b0dbe58
Patryk <patrykowo@gmail.com>
2022-11-11 23:11:50 +0100

    Import only essential to improve loading speed.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
-library.add(fas, far, fab)
+library.add(faGlobe, faSquareGithub, faEnvelope)
 dom.watch();
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
